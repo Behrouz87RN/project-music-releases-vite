87623fd61dccde9d9abab42603acd28ac46aacf8
Behrouz87RN <112555779+Behrouz87RN@users.noreply.github.com>
2023-10-22 21:01:06 +0200

    app.jsx

---
+++ src/Component/ArtistName.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArtistName = ({ artists }) => {
+  return <p>{
+    artists.map((artist) => {
+        return <a href={artist.href}>{artist.name}, </a>;
+    })
+  }</p>;
+};
+
+export default ArtistName;
---
+++ src/Component/CoverImage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const CoverImage = ({ src, alt }) => {
+  return <img src={src} alt={alt} />;
+};
+export default CoverImage;
